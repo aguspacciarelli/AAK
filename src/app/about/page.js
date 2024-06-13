f7171af2432b31d60bb67dceff73915764a4ba06
kaasch <karensch04@gmail.com>
2024-06-13 19:46:54 -0300

    agregue la pagina de contact y modifique el about us

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,14 +1,17 @@
 import CardsGrid from "@/app/components/CardsGrid/CardsGrid";
 import Card from "@/app/components/Card/Card";
-import Hero from"@/app/components/Hero/Hero"
+import Hero from"@/app/components/Hero/Hero";
+import styles from "@/app/about/page.module.css"
 
 const page = () => {
   return (
     <main>
-      <h1>About us!</h1>
-      <div>
-
-      </div>
+        <div className={styles.contenedor} >
+           <h1>About us</h1>  
+           <p>As an academic art institution, research and education are key to the Harvard Art Museums. Through art, the museums create powerful opportunities for the production, presentation, and interpretation of original scholarship. By offering unparalleled access to our collections and resources, the museums foster new approaches to the study and appreciation of art.</p>
+       
+        </div>
+       
     </main>
   );
 }
---
+++ src/app/contact/page.js
@@ -0,0 +1,15 @@
+import CardsGrid from "@/app/components/CardsGrid/CardsGrid";
+import Card from "@/app/components/Card/Card";
+import Hero from"@/app/components/Hero/Hero"
+
+const page = () => {
+  return (
+    <main>
+        <div>
+           <h1>Contact</h1>  
+        </div>
+    </main>
+  );
+}
+
+export default page;
