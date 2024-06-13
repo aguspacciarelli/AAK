136b1a58b8319fc62f3c6233d052e33687146960
assolucionesmultimediales <multimediales.solutions@gmail.com>
2024-06-13 13:03:58 -0300

    se quito el overflow del global css y se modifico el footer

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -1,7 +1,25 @@
+import styles from './Footer.module.css';
+import Image from 'next/image';
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <div className={styles.contenedorgral}>
+      <Image src="/logo.svg" alt="Descripción de la imagen" width={300} height={100} />
+      <nav className={styles.nav}>
+        <ul className={styles.ul}>
+          <li className={styles.li}>
+            <a href="#contact" className={styles.a}>Contact Us</a>
+          </li>
+          <li className={styles.li}>
+            <a href="#social" className={styles.a}>Social Media</a>
+          </li>
+          <li className={styles.li}>
+            <a href="#privacy" className={styles.a}>Privacy Policy</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
